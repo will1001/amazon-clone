bd368e08c346024f758d4f643f689572e66606cc
wili rahmat muhamad <wilirahmatm@gmail.com>
2020-10-25 11:03:20 +0800

    create header component

---
+++ src/component/Header/Header.js
@@ -0,0 +1,98 @@
+import React from 'react';
+
+// css
+import styles from './Header.module.css';
+
+// icon
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import MenuIcon from '@material-ui/icons/Menu';
+import RoomIcon from '@material-ui/icons/Room';
+
+function Header() {
+  return (
+    <>
+      <div className={styles.headerTop}>
+        <div className={styles.logo}>
+          <div className={styles.sideMenuButton}>
+            <MenuIcon/>
+          </div>
+          <img src="https://onlinebusinessmanager.com/wp-content/uploads/2018/09/white-amazon-logo-png-6.png" alt="logo" />
+        </div>
+        <div className={styles.search}>
+            <select select name="" id="">
+              <option value="">all</option>
+            </select>
+            <input className={styles.searchIn} type="text"/>
+            <div className={styles.icon}>
+              <SearchIcon/>
+            </div>
+        </div>
+        <div className={styles.nav}>
+            <div className={styles.option}>
+              <div className={styles.languageOption}>
+                <img className={styles.languageFlag} src="https://cdn3.iconfinder.com/data/icons/all-national-flags-of-the-world-very-high-quality/283/usa-512.png" alt="language" />
+                <ArrowDropDownIcon />
+              </div>
+            </div>
+            <div className={styles.option}>
+                <span className={styles.optionLineOne}>Hello, Sign in</span>
+                <span className={styles.optionLineTwo}>Account & List</span>
+            </div>
+            <div className={styles.option}>
+                <span className={styles.optionLineOne}>Return</span>
+                <span className={styles.optionLineTwo}>& Orders</span>
+              </div>
+            <div className={styles.option}>
+                <span className={styles.optionLineOne}>0</span>
+                <span><ShoppingCartIcon/> Cart</span>
+            </div>  
+        </div>
+      </div>
+      
+      <div className={styles.HeaderBottom}>
+        <div className={styles.deliverTo}>
+          <span className={styles.icon}><RoomIcon /></span>
+          <div className={styles.locationLabel}>
+            <div className={styles.label}>Deliver To</div>
+            <div className={styles.location}>Indonesia</div>
+          </div>
+        </div>
+        <div className={styles.nav}>
+          <div className={styles.option}>
+                <span>Today's Deal</span>
+          </div> 
+          <div className={styles.option}>
+                <span>wili's Amazon.com</span>
+          </div> 
+          <div className={styles.option}>
+                <span>Customer Service</span>
+          </div> 
+          <div className={styles.option}>
+            <select name="" id="">
+              <option value="">Browsing History</option>
+            </select>
+          </div> 
+          <div className={styles.option}>
+            <span>Buy Again</span>
+          </div> 
+          <div className={styles.option}>
+            <span>Gift Cards</span>
+          </div> 
+          <div className={styles.option}>
+            <span>Sell</span>
+          </div> 
+          <div className={styles.option}>
+            <span>Registry</span>
+          </div> 
+        </div>
+        <div className={styles.amazonCovid}>
+        <a href="">Amazon's response to COVID-19</a>
+      </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCXc5CUUti2HiQeAh8d_vfUigv-7UN-4bc",
+  authDomain: "clone-9aed9.firebaseapp.com",
+  databaseURL: "https://clone-9aed9.firebaseio.com",
+  projectId: "clone-9aed9",
+  storageBucket: "clone-9aed9.appspot.com",
+  messagingSenderId: "299382055567",
+  appId: "1:299382055567:web:f23fdcfef2316c7db8c6fa",
+  measurementId: "G-L74QCCFBVR"
+};
